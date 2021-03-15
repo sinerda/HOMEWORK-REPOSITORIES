aff4f391cebd4fefe847030c53e6807819f18ed8
sinerda <73904039+sinerda@users.noreply.github.com>
2021-03-15 17:12:59 +0200

    Slick Slider

---
+++ lesson 31/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    dots: true,
+  });
+});
